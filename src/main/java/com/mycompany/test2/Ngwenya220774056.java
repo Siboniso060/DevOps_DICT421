37c985f3a4d8bcf2851e1e007d14973825c3e335
Siboniso <siboniso064@gmail.com>
2024-05-07 14:58:04 +0200

    First commit Test2

---
+++ src/main/java/com/mycompany/test2/Ngwenya220774056.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test2;
+
+/**
+ *
+ * @author user
+ */
+public class Ngwenya220774056 {
+    public static void main(String[] args){
+        int num1 = 40;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
